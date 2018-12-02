050cefce7e68a5b78ed8616c3377dd994fb58aa0
modelorona <angel.hristozov@gmail.com>
2018-12-02 16:45:00 +0000

    separated theme into its own file

---
+++ client/src/components/Theme.js
@@ -0,0 +1,15 @@
+// modify this to change the theme of the chatbot
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Swiss 721 BT',
+  headerBgColor: '#005398',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#005398',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
+
+export default theme;
