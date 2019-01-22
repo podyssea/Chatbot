540369805573a656cda3bee11bcf19ca4f5313c1
modelorona <angel.hristozov@gmail.com>
2019-01-22 20:30:43 +0000

    work work work work work

--- deploy/updateServer.sh
+++ deploy/updateServer.sh
@@ -14,13 +14,13 @@ cd /home/ubuntu/dissertation/client
 npm install
 npm run build
 
-source ~/projectenv/bin/activate
+#source ~/projectenv/bin/activate
 cd ~/dissertation/server
 fuser -k -n tcp 5000
-pip install -r requirements.txt
-export FLASK_APP=app.py
-export FLASK_ENV=production
-flask run &> output.log &
+pip3 install -r requirements.txt
+#export FLASK_APP=app.py
+#export FLASK_ENV=production
+python3 app.py &> output.log &
 
 exit
 #sudo service nginx reload
