6ed7b90ecb14f79a0024509b53759ecc528eb0d0
modelorona <angel.hristozov@gmail.com>
2019-01-22 19:01:45 +0000

    fixed requirements

--- server/utils/intent_handler.py
+++ server/utils/intent_handler.py
@@ -17,8 +17,6 @@ def handle(data):
         return all_course_titles()
     elif data == 'Subject Areas':
         return all_subjects()
-    elif data[0: 20] == 'courses for subject ':
-        return specific_subject_courses(str(data).split('courses for subject ')[1])
 
 
 # go into the database and get a list of all available courses
