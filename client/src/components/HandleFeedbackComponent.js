c4aa76dc069a32e80b787005a014dc33dfa18bee
Anguel Hristozov <angel.hristozov@gmail.com>
2019-01-26 23:49:05 +0000

    typeform feedback form

--- client/src/components/HandleFeedbackComponent.js
+++ client/src/components/HandleFeedbackComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/functions';
+import parse from 'html-react-parser';
 import StarRatingComponent from 'react-star-rating-component';
 
 
@@ -16,6 +17,7 @@ class HandleFeedbackComponent extends Component {
 
         this.triggerNext = this.triggerNext.bind(this);
     }
+
 
     onStarClick(nextValue, prevValue, name) {
         this.setState({rating: nextValue, editable: false});
@@ -37,17 +39,22 @@ class HandleFeedbackComponent extends Component {
 
     render() {
         const { rating, editable } = this.state;
+        let styles = {
+            height: '300px',
+            width: '100%'
+        };
         // https://github.com/voronianski/react-star-rating-component this was used
         return (
-            <div className="handlefeedbackcomponent">
-                <h1>
-                <StarRatingComponent
-                    name="feedbackRating"
-                    starCount={5}
-                    value={rating}
-                    editing={editable}
-                    onStarClick={this.onStarClick.bind(this)}
-                /></h1>
+            <div className="handlefeedbackcomponent" style={styles}>
+                {/*<h1>*/}
+                {/*<StarRatingComponent*/}
+                    {/*name="feedbackRating"*/}
+                    {/*starCount={5}*/}
+                    {/*value={rating}*/}
+                    {/*editing={editable}*/}
+                    {/*onStarClick={this.onStarClick.bind(this)}*/}
+                {/*/></h1>*/}
+                {parse('<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://externalrelations3.typeform.com/to/fAkmQs"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>')}
             </div>
         );
     }
