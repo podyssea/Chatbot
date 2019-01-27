f78419abeec7632a0498dd43d6cfd7db40c5191c
Anguel <angel.hristozov@gmail.com>
2019-01-27 11:59:50 +0000

    cleaned up feedback file

--- client/src/components/HandleFeedbackComponent.js
+++ client/src/components/HandleFeedbackComponent.js
@@ -1,33 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import firebase from 'firebase/app';
-import 'firebase/functions';
 import parse from 'html-react-parser';
-import StarRatingComponent from 'react-star-rating-component';
-
 
 class HandleFeedbackComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            rating: 5,
-            editable: true,
-            comment: "", // for now, comment is empty until it is implemented as an element
-        };
 
         this.triggerNext = this.triggerNext.bind(this);
-    }
-
-
-    onStarClick(nextValue, prevValue, name) {
-        this.setState({rating: nextValue, editable: false});
-        let submitFeedback = firebase.functions().httpsCallable('feedback');
-
-        submitFeedback({comment: this.state.comment, rating: nextValue}).then((res) => {
-            console.log('feedback saved');
-        });
-
-        this.triggerNext();
     }
 
     triggerNext(value, trigger) {
@@ -38,22 +17,12 @@ class HandleFeedbackComponent extends Component {
 
 
     render() {
-        const { rating, editable } = this.state;
         let styles = {
-            height: '300px',
+            height: '340px',
             width: '100%'
         };
-        // https://github.com/voronianski/react-star-rating-component this was used
         return (
             <div className="handlefeedbackcomponent" style={styles}>
-                {/*<h1>*/}
-                {/*<StarRatingComponent*/}
-                    {/*name="feedbackRating"*/}
-                    {/*starCount={5}*/}
-                    {/*value={rating}*/}
-                    {/*editing={editable}*/}
-                    {/*onStarClick={this.onStarClick.bind(this)}*/}
-                {/*/></h1>*/}
                 {parse('<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://externalrelations3.typeform.com/to/fAkmQs"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>')}
             </div>
         );
