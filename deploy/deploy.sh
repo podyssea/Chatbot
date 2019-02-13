575293e617388134f7e031427c91428e492fead2
Anguel Hristozov <2255541h@student.gla.ac.uk>
2019-02-13 13:07:21 +0000

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,14 +10,6 @@ DEPLOY_SERVERS=$DEPLOY_SERVERS
 ALL_SERVERS=(${DEPLOY_SERVERS//,/ })
 echo "ALL_SERVERS ${ALL_SERVERS}"
 
-# echo in lines to create flaskenv file on the server
-sed -i '2icat .flaskenv' ./deploy/updateServer.sh
-sed -i '2iecho SECRET_KEY="$SECRET_KEY" >> .flaskenv' ./deploy/updateServer.sh
-sed -i '2iecho FLASK_APP="$FLASK_APP" >> .flaskenv' ./deploy/updateServer.sh
-sed -i '2iecho FLASK_ENV="$FLASK_ENV" >> .flaskenv' ./deploy/updateServer.sh
-sed -i '2iecho DATABASE_URL="$DATABASE_URL" >> .flaskenv' ./deploy/updateServer.sh
-sed -i '2i touch .flaskenv' ./deploy/updateServer.sh
-
 for server in "${ALL_SERVERS[@]}"
 do
   echo "deploying to ${server}"
