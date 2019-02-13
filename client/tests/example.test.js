4561e807ee84e7b15b1ba712b80d8cc8e67bb630
Hannah Mehravari <hannahmehravari@localhost.localdomain>
2019-02-13 08:53:28 +0000

    created necessary config files for enzyme and jest and npm run test now executes successfully

---
+++ client/tests/example.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+//import HandleInputComponent from 'HandleInputComponent'
+
+test('Correct feedback/error is sent depending on error code', () => {
+
+    let x = 4
+
+    expect(x).toBe(4);
+
+  });
