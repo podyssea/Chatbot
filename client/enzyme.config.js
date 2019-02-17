a61dcc5d05e5bc1185e1eb8b13879f38ff0ba524
Hannah Mehravari <hannahmehravari@glaroam2-154-202.wireless.gla.ac.uk>
2019-02-17 13:46:36 +0000

    basic tests written for chat window functionality

--- client/enzyme.config.js
+++
@@ -1,5 +0,0 @@
-import * as Enzyme from "enzyme";
-import * as EnzymeAdapter from "enzyme-adapter-react-16";
-// at the top of file , even  , before importing react
-// Setup enzyme"s react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
