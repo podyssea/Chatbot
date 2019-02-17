027aea530d422368d667b87503f1f43da5fea62d
Hannah Mehravari <hannahmehravari@glaroam2-154-202.wireless.gla.ac.uk>
2019-02-17 14:34:03 +0000

    completed tests for text-to-speech functionality

--- client/cypress/integration/text-to-speech.spec.js
+++ client/cypress/integration/text-to-speech.spec.js
@@ -4,11 +4,25 @@ describe('Text-to-speech is enabled and disabled correctly', function () {
        cy.get('.rsc-float-button').click();
     });
 
+    it('Bot\'s text-to-speech is initially disabled', function () {
+        cy.window().its('ttsOn').should('equal', false);
+    });
+
+
     it('Bot recognises text-to-speech enabling prompt and acts accordingly', function () {
-        cy.get('.rsc-content').should('be.visible');
+        cy.get('.rsc-input').type('Enable text-to-speech');
+        cy.get('.rsc-submit-button').click();
+        cy.window().its('ttsOn').should('equal', true);
     });
 
     it('Bot recognises text-to-speech disabling prompt and acts accordingly', function () {
+
+        cy.get('.rsc-input').type('Enable text-to-speech');
+        cy.get('.rsc-submit-button').click();
+        cy.get('.rsc-input').type('Disable text-to-speech');
+        cy.get('.rsc-submit-button').click();
+        cy.window().its('ttsOn').should('equal', false);
+
     });
 
 });
