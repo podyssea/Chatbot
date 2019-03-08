d84fe52a34ad59201360033119eebe4c682509b7
Anguel Hristozov <2255541h@student.gla.ac.uk>
2019-03-08 12:33:03 +0000

    Switch to Django complete

---
+++ server/externalrelations/webhookserver/intent_handler.py
@@ -0,0 +1,264 @@
+from .models import ShortCourse
+
+
+def handle(data):
+    if data['intent'] == 'Available Courses':
+        return all_course_titles()
+    elif data['intent'] == 'Subject Areas':
+        return all_subjects()
+    elif data['intent'] == 'Subject area -> Title':
+        return specific_subject_courses(data['parameters']['Subject_area'])
+        # ----------------------------------------------------
+    elif data['intent'] == 'Title -> Class Code':
+        return title_give_classcode(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Cost':
+        return title_give_cost(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Credits':
+        return title_give_credits(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Description':
+        return title_give_description(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Duration':
+        return title_give_duration(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> End Date':
+        return title_give_end(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Start Date':
+        return title_give_start(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Subject Area':
+        return title_give_subarea(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Tutor':
+        return title_give_tutor(data['parameters']['Course'][0])
+    elif data['intent'] == 'Title -> Venue':
+        return title_give_venue(data['parameters']['Course'][0])
+        # -------------------------------------------------
+    elif data['intent'] == 'ID -> Title':
+        return id_give_title(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Cost':
+        return id_give_cost(data['parameters']['number'])
+    elif data['intent'] == 'ID -> Credits':
+        return id_give_credits(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Description':
+        return id_give_description(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Duration':
+        return id_give_duration(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> End Date':
+        return id_give_end(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Start Date':
+        return id_give_start(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Subject Area':
+        return id_give_subarea(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Tutor':
+        return id_give_tutor(data['parameters']['number'][0])
+    elif data['intent'] == 'ID -> Venue':
+        return id_give_venue(data['parameters']['number'][0])
+        # --------------------------------------------------
+    elif data['intent'] == 'Cost -> Title':
+        return cost_give_title(data['parameters']['unit-currency'][0]['amount'])
+    elif data['intent'] == 'Credits -> Title':
+        return credits_give_title(data['parameters']['Credits'][0])
+    elif data['intent'] == 'Duration -> Title':
+        return duration_give_title(data['parameters']['duration'][0]['amount'])
+    elif data['intent'] == 'End date -> Title':
+        return end_give_title(data['parameters']['date'][0])  # TODO: fix these to be proper datetime fields
+    elif data['intent'] == 'Start date -> Title':
+        return start_give_title(data['parameters']['date'][0])  # TODO: same as above. proper datetime!!!!
+    elif data['intent'] == 'Subject area -> Title':
+        return subarea_give_title(data['parameters']['Subject_area'])
+    elif data['intent'] == 'Tutor -> Title':
+        return tutor_give_title(data['parameters']['given-name'])
+    elif data['intent'] == 'Venue -> Title':
+        return venue_give_title(data['parameters']['location'][0].get('subadmin-area'))
+
+
+def all_course_titles():
+    data = ShortCourse.all_course_titles()
+    resp = 'All courses that are available to take are as follows: '
+    return '{}{}'.format(resp, ', '.join([row.Title for row in data]))
+
+
+def all_subjects():
+    data = ShortCourse.all_subjects()
+    resp = 'All subjects that are available are as follows: '
+    return '{}{}'.format(resp, ', '.join([row.get('Subject_area') for row in data]))
+
+
+def specific_subject_courses(subject):
+    data = ShortCourse.specific_subject_courses(subject)
+    resp = 'All courses that belong to {} are as follows: '.format(subject)
+    return '{}{}'.format(resp, ', '.join([row.Title for row in data]))
+
+
+# -----------------------TITLE-------------------------------------------------
+
+
+def title_give_cost(title):
+    data = ShortCourse.title_give_cost(title)
+    resp = 'The cost of this course is '
+    return "{}{}".format(resp, data.get('Cost'))
+
+
+def title_give_classcode(title):
+    data = ShortCourse.title_give_classcode(title)
+    resp = 'The class code of this course is '
+    return '{}{}'.format(resp, data.get('Class_code'))
+
+
+def title_give_credits(title):
+    data = ShortCourse.title_give_credits(title)
+    resp = 'The credits attached for this course is '
+    return '{}{}'.format(resp, data.get('Credits_attached'))
+
+
+def title_give_description(title):
+    data = ShortCourse.title_give_description(title)
+    resp = 'To explain this course better.. '
+    return '{}{}'.format(resp, data.get('Description'))
+
+
+def title_give_duration(title):
+    data = ShortCourse.title_give_duration(title)
+    resp = 'This course goes on for '
+    return '{}{} days'.format(resp, data.get('Duration'))
+
+
+def title_give_end(title):
+    data = ShortCourse.title_give_end(title)
+    resp = 'The end date of this course is '
+    return '{}{}'.format(resp, data.get('End_date'))
+
+
+def title_give_start(title):
+    data = ShortCourse.title_give_start(title)
+    resp = 'This course starts on '
+    return '{}{}'.format(resp, data.get('Start_date'))
+
+
+def title_give_subarea(title):
+    data = ShortCourse.title_give_subarea(title)
+    resp = 'The subject area for this course is '
+    return '{}{}'.format(resp, data.get('Subject_area'))
+
+
+def title_give_tutor(title):
+    data = ShortCourse.title_give_tutor(title)
+    resp = 'The tutor of this course is '
+    return '{}{}'.format(resp, data.get('Tutor'))
+
+
+def title_give_venue(title):
+    data = ShortCourse.title_give_venue(title)
+    return '{}'.format(data.get('Venue'))
+
+
+# -----------------------ID--------------------------------------
+
+def id_give_cost(class_code):
+    data = ShortCourse.id_give_cost(class_code)
+    resp = 'The cost of this course is '
+    return '{}{}'.format(resp, data.get('Cost'))
+
+
+def id_give_title(class_code):
+    data = ShortCourse.id_give_title(class_code)
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def id_give_credits(class_code):
+    data = ShortCourse.id_give_credits(class_code)
+    resp = 'The credits attached for this course is '
+    return '{}{}'.format(resp, data.get('Credits_attached'))
+
+
+def id_give_description(class_code):
+    data = ShortCourse.id_give_description(class_code)
+    resp = 'To explain this course better.. '
+    return '{}{}'.format(resp, data.get('Description'))
+
+
+def id_give_duration(class_code):
+    data = ShortCourse.id_give_duration(class_code).get('Duration')
+    resp = 'This course goes on for '
+    return '{}{} {}'.format(resp, data, 'day' if data == 1 else 'days')  # todo: day(s) depending on duration
+
+
+def id_give_end(class_code):  # todo: see if these incorporate both date and time, think they should
+    data = ShortCourse.id_give_end(class_code)
+    resp = 'The end date of this course is '
+    return '{}{}'.format(resp, data.get('End_date'))
+
+
+def id_give_start(class_code):  # todo: read todo for id_give_end above, applies here as well
+    data = ShortCourse.id_give_start(class_code)
+    resp = 'This course starts on '
+    return '{}{}'.format(resp, data.get('Start_date'))
+
+
+def id_give_subarea(class_code):
+    data = ShortCourse.id_give_subarea(class_code)
+    resp = 'The subject area of this course is '
+    return '{}{}'.format(resp, data.get('Subject_area'))
+
+
+def id_give_tutor(class_code):
+    data = ShortCourse.id_give_tutor(class_code)
+    resp = 'The tutor of this course is '
+    return '{}{}'.format(resp, data.get('Tutor'))
+
+
+def id_give_venue(class_code):
+    data = ShortCourse.id_give_venue(class_code)
+    return '{}'.format(data.get('Venue'))
+
+
+# --------------------------OTHER--------------------------------
+
+def cost_give_title(cost):
+    data = ShortCourse.cost_give_title(cost)
+    resp = 'The Title of this course(s) is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def credits_give_title(credits_attached):
+    data = ShortCourse.credits_give_title(credits_attached)
+    resp = 'The Title of this course(s) is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def duration_give_title(duration):
+    data = ShortCourse.duration_give_title(duration)
+    resp = 'The Title of this course(s) is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def end_give_title(end):
+    data = ShortCourse.end_give_title(end)
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def start_give_title(start):
+    data = ShortCourse.start_give_title(start)
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def subarea_give_title(subarea):
+    data = ShortCourse.subarea_give_title(subarea)
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def tutor_give_title(tutor):
+    data = ShortCourse.tutor_give_title(tutor)
+    if data is None:
+        return 'This tutor does not lead any course.'
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
+
+
+def venue_give_title(venue):
+    data = ShortCourse.venue_give_title(venue)
+    if data is None:
+        return 'There are no courses held in this venue.'
+    resp = 'The Title of this course is '
+    return '{}{}'.format(resp, data.get('Title'))
---
+++ server/externalrelations/webhookserver/models.py
@@ -0,0 +1,192 @@
+from django.db import models
+
+
+class ShortCourse(models.Model):
+    Subject_area = models.TextField(max_length=150)
+    Title = models.TextField(max_length=150)
+    Class_code = models.IntegerField()
+    Start_date = models.TextField(max_length=50)
+    End_date = models.TextField(max_length=50)
+    Start_time = models.DecimalField(max_digits=10, decimal_places=2)
+    End_time = models.DecimalField(max_digits=10, decimal_places=2)
+    Cost = models.DecimalField(max_digits=10, decimal_places=2)
+    Duration = models.IntegerField()
+    Tutor = models.TextField(max_length=120)
+    Venue = models.TextField()
+    Link_to_Course_specification = models.TextField()
+    Description = models.TextField(max_length=65535)
+    Credits_attached = models.IntegerField()
+    Language_Level_of_Study_links = models.TextField()
+    pk_id = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'Short_Courses'
+        managed = False  # to not override the tables
+        verbose_name = 'Short Course'
+        verbose_name_plural = 'Short Courses'
+
+    def __str__(self):
+        return self.Title
+
+    @classmethod
+    def all_course_titles(cls):
+        records = ShortCourse.objects.all()
+        return records
+
+    @classmethod
+    def all_subjects(cls):
+        records = ShortCourse.objects.values('Subject_area').distinct()
+        return records
+
+    @classmethod
+    def specific_subject_courses(cls, subject):
+        records = ShortCourse.objects.filter(Subject_area=subject)
+        return records
+
+    # -----------------------------------TITLE------------------------------------
+
+    @classmethod
+    def title_give_cost(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Cost').first()
+        return records
+
+    @classmethod
+    def title_give_classcode(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Class_code').first()
+        return records
+
+    @classmethod
+    def title_give_credits(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Credits_attached').first()
+        return records
+
+    @classmethod
+    def title_give_description(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Description').first()
+        return records
+
+    @classmethod
+    def title_give_duration(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Duration').first()
+        return records
+
+    @classmethod
+    def title_give_end(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('End_date').first()
+        return records
+
+    @classmethod
+    def title_give_start(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Start_date').first()
+        return records
+
+    @classmethod
+    def title_give_subarea(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Subject_area').first()
+        return records
+
+    @classmethod
+    def title_give_tutor(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Tutor').first()
+        return records
+
+    @classmethod
+    def title_give_venue(cls, title):
+        records = ShortCourse.objects.filter(Title=title).values('Venue').first()
+        print(records)
+        return records
+
+    # -------------------------------ID-------------------------------------
+
+    @classmethod
+    def id_give_cost(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Cost').first()
+        return records
+
+    @classmethod
+    def id_give_title(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Title').first()
+        return records
+
+    @classmethod
+    def id_give_credits(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Credits_attached').first()
+        return records
+
+    @classmethod
+    def id_give_description(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Description').first()
+        return records
+
+    @classmethod
+    def id_give_duration(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Duration').first()
+        return records
+
+    @classmethod
+    def id_give_end(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('End_date').first()
+        return records
+
+    @classmethod
+    def id_give_start(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Start_date').first()
+        return records
+
+    @classmethod
+    def id_give_subarea(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Subject_area').first()
+        return records
+
+    @classmethod
+    def id_give_tutor(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Tutor').first()
+        return records
+
+    @classmethod
+    def id_give_venue(cls, class_code):
+        records = ShortCourse.objects.filter(Class_code=class_code).values('Venue').first()
+        return records
+
+    # ------------------------------OTHER---------------------------
+
+    @classmethod
+    def cost_give_title(cls, cost):
+        records = ShortCourse.objects.filter(Cost=cost).values('Title').first()
+        return records
+
+    @classmethod
+    def credits_give_title(cls, credits_attached):
+        records = ShortCourse.objects.filter(Credits_attached=credits_attached).values('Title').first()
+        return records
+
+    @classmethod
+    def duration_give_title(cls, duration):
+        records = ShortCourse.objects.filter(Duration=duration).values('Duration').first()
+        return records
+
+    @classmethod
+    def end_give_title(cls, end):
+        records = ShortCourse.objects.filter(End_date=end).values('Title').first()
+        return records
+
+    @classmethod
+    def start_give_title(cls, start):
+        records = ShortCourse.objects.filter(Start_date=start).values('Title').first()
+        return records
+
+    @classmethod
+    def subarea_give_title(cls, subarea):
+        records = ShortCourse.objects.filter(Subject_area=subarea).values('Title').first()
+        return records
+
+    @classmethod
+    def tutor_give_title(cls, tutor):
+        records = ShortCourse.objects.filter(Tutor__startswith=tutor).values('Title').first()
+        return records
+
+    @classmethod
+    def venue_give_title(cls, venue):
+        records = ShortCourse.objects.filter(Venue=venue).values('Title').first()
+        print(records)
+        return records
