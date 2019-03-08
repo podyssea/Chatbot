9ad4a71df664a3a639a53e8e0d864399f56baccc
modelorona <angel.hristozov@gmail.com>
2019-03-08 23:10:56 +0000

    removed unnecessary firebase mail function related code

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -68,37 +68,3 @@ exports.message = functions.https.onCall((data, context) => {
     });
 
 });
-
-// stupid mail function that doesn't work. thanks google for your examples that never work
-// exports.mail = functions.https.onCall((data, context) => {
-//     // these are declared in the GCP console for functions in the mail function
-//
-//     const password = process.env.gmailpassword;
-//     const email = process.env.gmailemail;
-//     console.log(email);
-//     console.log(password);
-//
-//     const mailTransport = nodemailer.createTransport({
-//         service: 'Gmail',
-//         auth: {
-//             user: email,
-//             pass: password
-//         }
-//     });
-//
-//     const mailOptions = {
-//         to: email
-//     };
-//
-//     mailOptions.subject = data.name + ' has a question.';
-//     mailOptions.text = data.query;
-//
-//     return mailTransport.sendMail(mailOptions, (err, info) => {
-//         if (err) {
-//             console.log(err);
-//             return {resp: 'Failed to send', status: 500};
-//         }
-//         return {resp: 'Sent', status: 200};
-//     })
-// });
-
